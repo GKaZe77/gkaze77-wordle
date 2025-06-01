05740ab52a22a9a29fc4decd62e66052d6bbdf6f
GKaZe77 <gkaze77@gmail.com>
2025-06-01 00:27:55 -0700

    Update keyboard.js

--- components/keyboard.js
+++ components/keyboard.js
@@ -1,10 +1,11 @@
 // components/keyboard.js
 
 const keysLayout = [
-  'QWERTYUIOP',
-  'ASDFGHJKL',
-  'ENTERZXCVBNM←'
+  ['Q','W','E','R','T','Y','U','I','O','P'],
+  ['A','S','D','F','G','H','J','K','L'],
+  ['ENTER','Z','X','C','V','B','N','M','←']
 ];
+
 
 export function renderKeyboard(onKeyPress) {
   const keyboard = document.getElementById('keyboard');
